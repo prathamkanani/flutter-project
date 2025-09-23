07869b1920030066bfbe6920ea7beb47c9a54181
Pratham Kanani <prathamka@meditab.com>
2025-09-23 16:09:03 +0530

    Unit Testing Auth Service

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -2,12 +2,20 @@ import 'package:demo_project/services/auth/auth_provider.dart';
 import 'package:demo_project/services/auth/auth_user.dart';
 import 'package:demo_project/services/auth/firebase_auth_provider.dart';
 
+/// Auth Service is dependent on Auth Provider so,
+/// Auth Service is not making assumption that its always Firebase
+/// instead it provides factory for firebase (using dependency injection).
+
+/// DEPENDENCY INJECTION
+/// Auth service is dependent on a provider using a constant
+/// constructor initializer we are injecting the provider into it.
+
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
 
   factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
-  
+
   @override
   Future<void> initialize() => provider.initialize();
 
@@ -24,10 +32,8 @@ class AuthService implements AuthProvider {
   Future<void> logOut() => provider.logOut();
 
   @override
-  Future<AuthUser> login({
-    required String email, 
-    required String password,
-    }) => provider.login(email: email, password: password);
+  Future<AuthUser> login({required String email, required String password}) =>
+      provider.login(email: email, password: password);
 
   @override
   Future<void> sendEmailVerification() => provider.sendEmailVerification();
